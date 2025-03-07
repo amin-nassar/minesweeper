b251df0fff4ba50cca4abc7b9a5cd78661f3ddf9
amin-nassar <amin.nassar.ce@gmail.com>
2025-03-07 05:08:05 +0200

    Build board with numbers and mines

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,32 +1,12 @@
 import classes from "./styles.module.css";
 import Cell from "../Cell";
-import type { GameBoard } from "./types";
-
-const values: GameBoard = [
-  [
-    { value: 7, isFlagged: true, isOpen: false },
-    { value: "mine", isOpen: true, isFlagged: false },
-    { value: 1, isOpen: true, isFlagged: false },
-    { value: "mine", isFlagged: true, isOpen: false },
-  ],
-  [
-    { value: "mine", isOpen: true, isFlagged: false },
-    { value: 5, isFlagged: false, isOpen: true },
-    { value: 6, isOpen: false, isFlagged: false },
-    { value: 4, isOpen: true, isFlagged: false },
-  ],
-  [
-    { value: 2, isOpen: true, isFlagged: false },
-    { value: 0, isFlagged: false, isOpen: false },
-    { value: "mine", isFlagged: false, isOpen: false },
-    { value: 3, isOpen: true, isFlagged: false },
-  ],
-];
+import { createBoard } from "../../utils";
 
 export default function Board() {
+  const board = createBoard({ cols: 10, rows: 10, mines: 10 });
   return (
     <div className="board">
-      {values.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <div key={rowIndex} className={classes.row}>
           {row.map((cell, cellIndex) => (
             <Cell key={cellIndex} cell={cell} />
---
+++ src/utils/index.ts
@@ -0,0 +1,64 @@
+import { GameBoard } from "../components/Board/types";
+import { GameCell } from "../components/Cell";
+
+function createEmptyCell(): GameCell {
+  return { value: 0, isFlagged: false, isOpen: false };
+}
+
+function isMine(cell: GameCell) {
+  return cell.value === "mine";
+}
+
+function getAdjacentCells(board: GameBoard, row: number, col: number) {
+  const directions = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
+  const isValidPair = ([r, c]: number[]) => !!board[r]?.[c];
+
+  // TODO: Merge 2 .map calls together.
+  return directions
+    .map(([dr, dc]) => [row + dr, col + dc])
+    .filter(isValidPair)
+    .map(([r, c]) => board[r][c]);
+}
+
+type BoardInfo = { rows: number; cols: number; mines: number };
+export function createBoard({ rows, cols, mines }: BoardInfo): GameBoard {
+  const board = Array.from({ length: rows }, () =>
+    Array.from({ length: cols }, createEmptyCell)
+  );
+
+  let mineCount = 0;
+  while (mineCount < mines) {
+    const row = Math.floor(Math.random() * rows);
+    const col = Math.floor(Math.random() * cols);
+    const cell = board[row][col];
+    if (!isMine(cell)) {
+      (cell as GameCell).value = "mine";
+      mineCount++;
+    }
+  }
+
+  board.forEach((row, rIdx) => {
+    row.forEach((cell, cIdx) => {
+      if (cell.value === "mine") return;
+      // TODO: Convert `getAdjacentCells` to `countAdjacentMines`
+      const adjacentCells = getAdjacentCells(board, rIdx, cIdx);
+      // TODO: Fix Typing Or Extract Numeric Values of a cell
+      cell.value = adjacentCells.filter(isMine).length as Exclude<
+        GameCell["value"],
+        "mine"
+      >;
+    });
+  });
+
+  return board;
+}

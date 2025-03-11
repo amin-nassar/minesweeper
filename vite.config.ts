c7aad11857b033b0d639b2860a7520d3c590eab2
amin-nassar <amin.nassar.ce@gmail.com>
2025-03-12 01:21:43 +0200

    Add gh-pages to deploy project online.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/minesweeper",
   server: { port: 3000, open: true },
 });
